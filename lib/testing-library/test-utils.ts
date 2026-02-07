0d04bf9a8971bb8fe7f8c8c5e3ded0e7406a308e
Adonis Butufei <adonis.butufei@gmail.com>
2026-02-07 02:25:37 +0200

    09. Update explanations in test-utils

--- lib/testing-library/test-utils.ts
+++ lib/testing-library/test-utils.ts
@@ -2,7 +2,16 @@
 
 import type { ReactNode } from 'react'
 
-// Note: This is using the original @testing-library/react types and functions
+/*
+ Note: This is using the original @testing-library/react types and functions
+ It adds the custom queries and re-exports screen, render and within
+
+ In order to keep the import as @testing-library/react it used alias in vitest.config.ts and
+ tsconfig.json
+
+ This was necessary because the global sceen object is clashing with DOM window.screen and cannot be used in global scope
+*/
+
 import type { Screen, RenderOptions } from 'rtl-original'
 
 import {
